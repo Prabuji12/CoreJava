4a0d9f901d5dd226db4bb67d96a9d88a115c0136
Prabuji12 <sp12prabu@gmail.com>
2019-04-08 12:47:53 +0530

    third_commit_prabhu

---
+++ src/inheritance/Tiger.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+public class Tiger extends Animal {
+
+String tigerColor;
+	
+	public Tiger() {
+		System.out.println("Dog No Parameter Constructor");
+	}
+	
+	public Tiger(int height,int length,String color) {
+		super(height,length,color);
+		System.out.println("Tiger heght is "+height+" and length is "+length);
+		
+	}
+	
+	public void sound() {
+		super.sound();
+		System.out.println("Tiger will roar");
+	}
+
+	
+}
