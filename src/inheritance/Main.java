3e9a14b989bed2e6c8261883d97e3ff8d5c3aa1f
Prabuji12 <sp12prabu@gmail.com>
2020-01-29 10:34:19 +0530

    "recent change"

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -3,8 +3,8 @@ package inheritance;
 public class Main {
 	
 	public static void main(String[] args) {
-		Dog obj = new Dog(12,30,"White");
-		obj.sound();
+		Dog objName = new Dog(12,32,"White");
+		objName.sound();
 		//Dog obj1 = new Dog(5,20);
 	}
 
